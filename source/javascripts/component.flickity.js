d3f056e6327afa820a9752766cb7bd0e2dbb6a66
Callum Flack <callumflack@gmail.com>
2016-05-13 16:56:45 +1000

    reinstate work that SL wiped so magically

--- source/javascripts/component.flickity.js
+++ source/javascripts/component.flickity.js
@@ -7,40 +7,41 @@
 
 $(document).ready( function() {
 
-    var $gallery = $('.gallery').flickity({
+	var $gallery = $('.gallery').flickity({
 		arrowShape: {
-            x0: 10,
-            x1: 60, y1: 50,
-            x2: 63, y2: 50,
-            x3: 13
+			x0: 10,
+			x1: 60, y1: 50,
+			x2: 63, y2: 50,
+			x3: 13
 		},
-        pageDots: true,
+		pageDots: false,
 		wrapAround: true,
 		autoPlay: 5000,
-        lazyLoad: true,
+		lazyLoad: true,
 		pauseAutoPlayOnHover: false,
-        imagesLoaded: true
+		prevNextButtons: false,
+		imagesLoaded: true
 	});
 
-    // For adding captions to a flickity instance:
-    //
-    // <div class="gallery">
-    //     {% assign slide_images = assets | where: "meta.slide", "true" %}
-    //     {% for images in slide_images %}
-    //         <div class="gallery-cell" data-caption="{{ forloop.index }} of {{ forloop.length }} | {{images.meta.caption}}">
-    //             <img src="{{images.url}}" >
-    //         </div>
-    //     {% endfor %}
-    // </div>
-    // <p class="gallery-caption u-marginTX2">&nbsp;</p>
+	// For adding captions to a flickity instance:
+	//
+	// <div class="gallery">
+	//     {% assign slide_images = assets | where: "meta.slide", "true" %}
+	//     {% for images in slide_images %}
+	//         <div class="gallery-cell" data-caption="{{ forloop.index }} of {{ forloop.length }} | {{images.meta.caption}}">
+	//             <img src="{{images.url}}" >
+	//         </div>
+	//     {% endfor %}
+	// </div>
+	// <p class="gallery-caption u-marginTX2">&nbsp;</p>
 
-    var $caption = $('.gallery-caption');
+	var $caption = $('.gallery-caption');
 
-    // Flickity instance
-    var flkty = $gallery.data('flickity');
-    $gallery.on( 'cellSelect', function() {
-        // set image caption using img's alt
-        $caption.html( $( flkty.selectedElement ).data('caption') );
-    });
+	// Flickity instance
+	var flkty = $gallery.data('flickity');
+	$gallery.on( 'cellSelect', function() {
+		// set image caption using img's alt
+		$caption.html( $( flkty.selectedElement ).data('caption') );
+	});
 
 });
