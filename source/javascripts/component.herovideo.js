18e1fda70e522ff533681ba594cada1dca0c6e1b
callum@patternworks.com.au <callum@patternworks.com.au>
2016-06-03 16:16:52 +1000

    Updated Application.Min and 2 other files

--- source/javascripts/component.herovideo.js
+++ source/javascripts/component.herovideo.js
@@ -1,4 +1,5 @@
 
+
 // --------------------------------------------------------------------------
 // COMPONENT: HERO
 // Youtube embed poster frame reveal
@@ -7,37 +8,37 @@
 
 
 $(document).on('click','.js-videoPoster',function(ev) {
-    ev.preventDefault();
-    var $poster = $(this);
-    var $wrapper = $poster.closest('.js-videoWrapper');
-    videoPlay($wrapper);
+	ev.preventDefault();
+	var $poster = $(this);
+	var $wrapper = $poster.closest('.js-videoWrapper');
+	videoPlay($wrapper);
 });
 
 // play the targeted video (and hide the poster frame)
 function videoPlay($wrapper) {
-    var $iframe = $wrapper.find('.js-videoIframe');
-    var src = $iframe.data('src');
+	var $iframe = $wrapper.find('.js-videoIframe');
+	var src = $iframe.data('src');
 
-    // hide poster
-    $wrapper.addClass('videoWrapperActive');
-    // add iframe src in, starting the video
-    $iframe.attr('src',src);
+	// hide poster
+	$wrapper.addClass('videoWrapperActive');
+	// add iframe src in, starting the video
+	$iframe.attr('src',src);
 }
 
 // stop the targeted/all videos (and re-instate the poster frames)
 function videoStop($wrapper) {
-    // if we're stopping all videos on page
-    if (!$wrapper) {
-    var $wrapper = $('.js-videoWrapper');
-    var $iframe = $('.js-videoIframe');
-    // if we're stopping a particular video
-    } else {
-    var $iframe = $wrapper.find('.js-videoIframe');
-    }
-    // reveal poster
-    $wrapper.removeClass('videoWrapperActive');
-    // remove youtube link, stopping the video from playing in the background
-    $iframe.attr('src','');
+	// if we're stopping all videos on page
+	if (!$wrapper) {
+	var $wrapper = $('.js-videoWrapper');
+	var $iframe = $('.js-videoIframe');
+	// if we're stopping a particular video
+	} else {
+	var $iframe = $wrapper.find('.js-videoIframe');
+	}
+	// reveal poster
+	$wrapper.removeClass('videoWrapperActive');
+	// remove youtube link, stopping the video from playing in the background
+	$iframe.attr('src','');
 }
 
 
@@ -46,23 +47,23 @@ function videoStop($wrapper) {
 var $allVideos = $("iframe[src^='https://www.youtube.com']:not(.videoIframe)"),
 
 // The element that is fluid width
-$fluidEl = $(".Container.Container--textColumn");
+$fluidEl = $(".Container");
 
 // When the window is resized
 $(window).resize(function() {
 
-    var newWidth = $fluidEl.width();
+	var newWidth = $fluidEl.width();
 
-    // Resize all videos according to their own aspect ratio
-    $allVideos.each(function() {
+	// Resize all videos according to their own aspect ratio
+	$allVideos.each(function() {
 
-    var $el = $(this);
+	var $el = $(this);
 
-    var newHeight = newWidth * 0.56; // 16by9
+	var newHeight = newWidth * 0.56; // 16by9
 
-    if (newHeight <= $el.attr('height')) {
-        $el.height(newHeight);
-    }
+	if (newHeight <= $el.attr('height')) {
+		$el.height(newHeight);
+	}
 
 });
 
