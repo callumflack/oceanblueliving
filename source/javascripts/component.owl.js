85de29540b233226f2de31fab31b18c446d9043d
Callum Flack <callumflack@gmail.com>
2016-07-06 11:03:52 +1000

    home pg styling & images WIP

--- source/javascripts/component.owl.js
+++ source/javascripts/component.owl.js
@@ -7,7 +7,7 @@
 
 $(document).ready( function() {
 
-	var owl = $('.owl-carousel');
+	var owl = $('.js-owl');
 
 	owl.owlCarousel({
 		animateOut: 'fadeOut',
@@ -16,6 +16,18 @@ $(document).ready( function() {
 		// autoWidth:true,
 		items: 1,
 		loop: true,
+		smartSpeed: 1000
+	});
+
+	var owlDragger = $('.js-owl-dragger');
+
+	owlDragger.owlCarousel({
+		autoplay: true,
+		autoplayTimeout: 5000,
+		// autoWidth:true,
+		items: 1,
+		loop: true,
+		responsive: false,
 		smartSpeed: 1000
 	});
 
