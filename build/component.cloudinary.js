c81f03503be6e8323186d128b5100bcdb6418853
Callum Flack <callum@patternworks.com.au>
2017-03-25 20:46:55 +1000

    Updated Obl Screen Hillviewcres Purplepool Detail Bw and 12 other files

--- build/component.cloudinary.js
+++ build/component.cloudinary.js
@@ -1,4 +1,5 @@
 // https://codepen.io/geelen/pen/RGvQyJ
+// g_auto or g_center
 
 (() => {
     const baseUrl = 'http://res.cloudinary.com/pw-img-cdn/image/fetch'
@@ -9,7 +10,7 @@
         
         const { clientWidth, clientHeight } = image
         const pixelRatio = window.devicePixelRatio || 1.0
-        const imageParams = `w_${100 * Math.round(clientWidth * pixelRatio / 100)},h_${100 * Math.round(clientHeight * pixelRatio / 100)},c_fill,g_auto,f_auto`
+        const imageParams = `w_${100 * Math.round(clientWidth * pixelRatio / 100)},h_${100 * Math.round(clientHeight * pixelRatio / 100)},c_fill,g_center,f_auto`
         const url = `${baseUrl}/${imageParams}/${image.dataset.bg}`
         
         image.style.backgroundImage = `url('${url}')`
