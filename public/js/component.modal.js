ec7a10577d576e663dd49808670f39b12f1287e0
Callum Flack <callum@patternworks.com.au>
2018-04-16 12:08:19 +1000

    Updated 9 Sennfeld Cl and 49 other files

--- public/js/component.modal.js
+++ public/js/component.modal.js
@@ -3,24 +3,18 @@
   // See also Todd Motto's data attribute manipulations: https://goo.gl/dFE49n
   // Basics: http://stackoverflow.com/questions/2937227/what-does-function-jquery-mean
 
-  function closeModal() {
-    var MODAL = "[data-modal]",
-      ACTIVE_CLASS = "is-active";
-
-    $(MODAL).removeClass(ACTIVE_CLASS);
-  }
+  // Link two data-modal-toggle and data-modal elements by setting the same data-modal-id value on both
 
   // Show the modal in the DOM
   $(document).on("click", "[data-modal-toggle]", function(e) {
     e.stopPropagation();
 
-    // Close any open modals.
-    closeModal();
+    var id = e.currentTarget.getAttribute("data-modal-id");
 
-    var MODAL = "[data-modal]",
+    var MODAL = "[data-modal-id=" + id + "]",
       ACTIVE_CLASS = "is-active";
 
-    $(MODAL).addClass(ACTIVE_CLASS);
+    $(MODAL).toggleClass(ACTIVE_CLASS);
 
     // Clicking anywhere outside the modal nav closes the modal.
     // Only attaches if a modal has been triggered.
@@ -29,9 +23,9 @@
       var MODAL = "[data-modal]",
         ACTIVE_CLASS = "is-active";
 
-      // If any modal is visible, close it.
+      // If a modal is visible, close it.
       if ($(MODAL).hasClass(ACTIVE_CLASS)) {
-        closeModal();
+        $(MODAL).removeClass(ACTIVE_CLASS);
       }
 
       $(document).unbind("click.modalNav");
