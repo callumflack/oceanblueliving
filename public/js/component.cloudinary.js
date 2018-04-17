552722227712f152cd59ee6521ad730dc0227c7e
Callum Flack <callumflack@gmail.com>
2018-04-17 13:43:48 +1000

    fix: cloudinary loading on applications pg

--- public/js/component.cloudinary.js
+++ public/js/component.cloudinary.js
@@ -14,17 +14,19 @@
       Math.round(clientHeight * pixelRatio / 100)},c_fill,g_center,f_auto`;
     const url = `${baseUrl}/${imageParams}/${image.dataset.bg}`;
 
-    image.style.backgroundImage = `url('${url}')`;
     // image.src = url
+    image.style.backgroundImage = `url('${url}')`;
   });
 
   // https://jsfiddle.net/idobarnoam/k89ff3ay/1/
   Array.from(document.querySelectorAll(".js-cloudinaryImage")).forEach(
     image => {
+      // for images
       // image.addEventListener('load', () => {
       //     image.classList.add('is-loaded')
       // })
-      // hack for bg images
+
+      // for bg images
       setTimeout(function() {
         image.classList.add("is-loaded");
       }, 1500);
